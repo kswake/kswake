850e466a034d421dd9a54f3ae370777ba30859fa
kswake <kbschindler@my.waketech.edu>
2024-09-07 22:03:09 -0400

    dfgsdfgsdgfds

--- binaryAdd.py
+++ binaryAdd.py
@@ -6,12 +6,12 @@ import array
 bin1 = [0,1,1,0,1,0,0,1]
 bin2 = [0,1,0,0,1,1,1,0]
 out1 = [0,0,0,0,0,0,0,0]
+val1 = [128,64,32,16,8,4,2,1]
 carry = 0
 
-print("bin1  |    bin2   |  result   |    carry")
+print("bin1  |    bin2   |  result   |    carry   ||    value    ")
 
 for i in range (7,-1,-1): 
-    
     if carry == 1:
         result = 1
         carry = 0
@@ -21,15 +21,9 @@ for i in range (7,-1,-1):
             result = 0
         else:
             result = bin1[i]+bin2[i]
+
     out1[i] = result
-    
-    print(str(bin1[i])+"     |     "+str(bin2[i])+"     |     "+str(result)+"     |     "+str(carry))
 
-print(bin1)
-print("     +     ")
-print(bin2)
-print("     =     ")
-print(out1)
-
+    print(str(bin1[i])+"     |     "+str(bin2[i])+"     |     "+str(result)+"     |     "+str(carry)+"      ||     "+str(val1[i]))
 
 
